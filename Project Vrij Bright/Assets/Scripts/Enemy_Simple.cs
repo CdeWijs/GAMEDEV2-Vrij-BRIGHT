01e4332cc63ddd8b7433de4770af62bd4f1f2c05
marlonsijnesael <fakiefsflip@gmail.com>
2018-05-25 20:48:14 +0200

    minor changes

---
+++ Project Vrij Bright/Assets/Scripts/Enemy_Simple.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Simple : EnemyBaseClass {
+
+    private Transform targetTransform;
+    private SpriteRenderer sprR;
+
+    new public void Start(){
+        base.Start();
+        sprR = GetComponentInChildren<SpriteRenderer>();
+    }
+    new public void Update(){
+        EnemyMovement();
+    }
+
+    //enemy moves towards target 
+    public override void EnemyMovement() {
+        float xPos = Mathf.Sin(Time.time * 1.25f);
+        
+        if (xPos < 0){
+            sprR.flipX = true;
+        }
+
+        else{
+            sprR.flipX = false;
+        }
+
+        Vector3 moveToPos = new Vector3(transform.position.x + xPos, transform.position.y, 0);
+        transform.position = Vector2.MoveTowards(transform.position, moveToPos, moveSpeed * Time.deltaTime);
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision){
+        if (collision.transform.tag == "Player"){
+            playerObject.GetComponent<BoyClass>().health -= 15;
+        }
+    }
+}
