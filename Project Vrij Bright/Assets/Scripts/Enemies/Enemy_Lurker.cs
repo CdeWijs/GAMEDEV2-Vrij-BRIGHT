730f291797d8f8d5cd8b88f944a5afce33e5f8f3
BastDrum21 <bastiaanvanbentum@gmail.com>
2018-06-09 13:58:47 +0200

    music sound design balance

--- Project Vrij Bright/Assets/Scripts/Enemies/Enemy_Lurker.cs
+++ Project Vrij Bright/Assets/Scripts/Enemies/Enemy_Lurker.cs
@@ -33,7 +33,7 @@ public class Enemy_Lurker : EnemyBaseClass {
 		instance = FMODUnity.RuntimeManager.CreateInstance (eventRef);
 		FMODUnity.RuntimeManager.PlayOneShotAttached (eventRef, this.gameObject);
 		FMODUnity.RuntimeManager.AttachInstanceToGameObject (instance, this.gameObject.transform, rigidBody2D);
-		instance.start ();;
+		instance.start ();
 		instance.getParameter ("MonsterStatus", out monsterStatus);
 		monsterStatus.setValue (dwalen);
     }
