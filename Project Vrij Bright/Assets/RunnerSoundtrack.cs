714ea4e082f7dbbe60f5774f7067e6eb287ca0e5
Celine de Wijs <31764854+CdeWijs@users.noreply.github.com>
2018-08-27 10:03:38 +0200

    Added Runner Soundtrack and Reordered Levels

---
+++ Project Vrij Bright/Assets/RunnerSoundtrack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunnerSoundtrack : MonoBehaviour {
+
+    public BoyClass boyClass;
+    public GameObject endSoundtrack;
+
+    private void Update()
+    {
+        if (boyClass.health <= 15)
+        {
+            endSoundtrack.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+}
